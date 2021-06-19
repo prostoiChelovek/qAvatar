e6dfea1d41db084eff0c0419b9e45dbc5cd5cc96
prostoichelovek <i.prostoi.chelovek@yandex.ru>
2021-06-19 14:25:20 +0500

    create a simple test program to print readings from loadcell

---
+++ experiment/loadcell-first-test/src/main.cpp
@@ -0,0 +1,31 @@
+#include <Arduino.h>
+
+#include "HX711.h"
+
+const int LOADCELL_DOUT_PIN = 2;
+const int LOADCELL_SCK_PIN = 3;
+
+HX711 scale;
+
+void setup() {
+    Serial.begin(115200);
+
+    scale.begin(LOADCELL_DOUT_PIN, LOADCELL_SCK_PIN);
+    scale.tare();
+}
+
+unsigned long start;
+void loop() {
+    if (scale.is_ready()) {
+        auto value = static_cast<long>(scale.read());
+
+        unsigned long end = millis();
+        unsigned long elapsed = end - start;
+        start = millis();
+
+
+        Serial.print(value);
+        Serial.print(" ");
+        Serial.println(elapsed);
+    }
+}
